e2502e2741522e2967ddb0f9010221dade544423
VitorHSilver <vitor-henrique1996@hotmail.com>
2024-11-28 13:27:13 -0300

    MVC

--- server.js
+++ server.js
@@ -1,55 +1,12 @@
 import express from 'express';
-// 3000 é a porta padrao é para servidor
-
-const posts = [
-     {
-          id: 1,
-          descricao: 'Uma foto teste',
-          imagem: 'https://placecats.com/millie/300/150',
-     },
-     {
-          id: 2,
-          descricao: 'Gato brincando com um novelo de lã',
-          imagem: 'https://placecats.com/millie/300/151',
-     },
-     {
-          id: 3,
-          descricao: 'Gatinho dormindo em uma caixa',
-          imagem: 'https://placecats.com/millie/300/152',
-     },
-     {
-          id: 4,
-          descricao: 'Gato olhando pela janela',
-          imagem: 'https://picsum.photos/id/237/200/300',
-     },
-     {
-          id: 5,
-          descricao: 'Gato comendo ração',
-          imagem: 'https://placecats.com/millie/300/154',
-     },
-     {
-          id: 6,
-          descricao: 'Gato preto em um beco',
-          imagem: 'https://placecats.com/millie/300/155',
-     },
-];
+import routes from './src/route/postsRoute.js';
 
 const app = express();
-app.use(express.json());
+routes(app)
+
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
      console.log('servidor estudando');
 });
-app.get('/posts', (req, res) => {
-     res.status(200).json(posts);
-});
 
-function buscarPostPorId(id) {
-     return posts.findIndex((post) => {
-          return post.id === Number(id);
-     });
-}
 
-app.get('/posts/:id', (req, res) => {
-     const index = buscarPostPorId(req.params.id);
-     res.status(200).json(posts[index]);
-});
---
+++ src/models/postsModel.js
@@ -0,0 +1,13 @@
+import conectarAoBanco from "../config/dbconfig.js"
+// Conecta ao banco de dados MongoDB usando a string de conexão fornecida no ambiente
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
+
+// Função assíncrona para obter todos os posts do banco de dados
+export async function getTodosPosts() {
+     // Obtém o banco de dados 'api-instabytes'
+     const db = conexao.db('api-instabytes');
+     // Obtém a coleção 'posts'
+     const colecao = db.collection('posts');
+     // Retorna um array com todos os documentos da coleção
+     return colecao.find().toArray();
+}
